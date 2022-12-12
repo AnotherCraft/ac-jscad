836570e221b19431a884c6cc672b8132b2da3592
Danol <czdanol@gmail.com>
2022-12-12 11:20:04 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# OpenJSCAD
+This is a package of OpenJSCAD CLI interface: https://github.com/jscad/OpenJSCAD.org/blob/master/packages/cli/README.md
+
+You'll need Node.js and pkg. Simply do:
+```
+pkg . -t host
+```
+
+The resulting binary should be put into `ac_project/bin/bin_common_XXX`.
---
+++ cli.js
@@ -0,0 +1,10 @@
+require("./node_modules/@jscad/modeling");
+require("./node_modules/@jscad/cli/cli.js");
+/*
+const p = require('path');
+const fs = require('fs');
+console.log(p.resolve(__dirname));
+
+fs.readdirSync("C:/snapshot/openjscad/node_modules").forEach(file => {
+  console.log(file);
+});*/
